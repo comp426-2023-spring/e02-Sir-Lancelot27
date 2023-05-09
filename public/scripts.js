d97d1d1b7a70467509c6692c4766bf4e75ad8f83
Sir-Lancelot27 <whitleylance@gmail.com>
2023-05-09 05:50:31 -0400

    Added choice selection with radio clicks and buttons at bottom

--- public/scripts.js
+++ public/scripts.js
@@ -2,17 +2,31 @@
 // check out the coin-server example from a previous COMP 426 semester.
 // https://github.com/jdmar3/coinserver
 
+function selection() {
+    var rpsls = document.getElementById('rpsls');
+
+    var opp = document.getElementById('opp');
+
+    if (opp.checked) {
+        document.getElementById('rpsShots').style.display = "block";
+        if (rpsls.checked)
+        {
+            document.getElementById('rpslsShots').style.display = "block";
+        }
+        else {
+            document.getElementById('rpslsShots').style.display = "none";
+        }
+    }
+}
+
 function play() {
- 
-}
-
-function showShots() {
 
 }
 
-function showRules() {
+function reset() {
 
 }
-function restart() {
 
+function rules() {
+    
 }
