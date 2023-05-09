b8efb4833f6c1741f27186dd38fdacdf1d7c54cf
Sir-Lancelot27 <whitleylance@gmail.com>
2023-05-09 06:21:59 -0400

    added no opponent results

--- public/scripts.js
+++ public/scripts.js
@@ -2,6 +2,8 @@
 // check out the coin-server example from a previous COMP 426 semester.
 // https://github.com/jdmar3/coinserver
 
+
+// Handles button appearance for selected items
 function selection() {
     var rpsls = document.getElementById('rpsls');
 
@@ -19,8 +21,34 @@ function selection() {
     }
 }
 
-function play() {
+// Logic for game play with requests to api
+async function play() {
+    var baseurl = window.location.href.concat('app');
 
+    if (document.getElementById('rpsls').checked) {
+        baseurl = baseurl.concat('/rpsls');
+    }
+    else {
+        baseurl = baseurl.concat('/rps');
+    }
+
+    document.getElementById('play').style.display = "none";
+    document.getElementById('selection').style.display = "none";
+    document.getElementById('rules').style.display = "none";
+    var url = baseurl.concat('/play/');
+
+    if (!document.getElementById('opp').check) {
+        let response = await fetch(url);
+        let result = await response.json();
+        console.log(result.player);
+        document.getElementById("results").innerHTML = result.player;
+    }
+    else {
+
+        url = url.concat();
+    }
+    
+    document.getElementById("results").style.display = 'block';
 }
 
 function reset() {
@@ -28,5 +56,5 @@ function reset() {
 }
 
 function rules() {
-    
+
 }
