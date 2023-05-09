ba9fccee1c1b70b38bf48fa89d43a819c163f761
Sir-Lancelot27 <whitleylance@gmail.com>
2023-05-09 05:23:30 -0400

    fixed buttons

--- public/scripts.js
+++ public/scripts.js
@@ -2,31 +2,8 @@
 // check out the coin-server example from a previous COMP 426 semester.
 // https://github.com/jdmar3/coinserver
 
-
-// Function to uncheck the alternate radio if one is clicked.
-// For example iff RPS is clicked RPSLS would unclick.
-function swapClicks() {
-    var rps = document.getElementById('rps').checked;
-    var rpsls = document.getElementById('rpsls').checked;
-    var rd = document.getElementById('rd').checked;
-    var opp = document.getElementById('opp').checked;
-
-    if (rps)
-        document.getElementById('rpsls').prop("checked", false);
-    else
-        document.getElementById('rps').prop("checked", false);
-    if (rd)
-        document.getElementById('rd').prop("checked", false);
-    else
-        document.getElementById('opp').prop("checked", false);
-}
-
 function play() {
-    // See which radios are selected
-    var rps = document.getElementById('rps').checked;
-    var rpsls = document.getElementById('rpsls').checked;
-    var rd = document.getElementById('rd').checked;
-    var opp = document.getElementById('opp').checked;
+ 
 }
 
 function showShots() {
